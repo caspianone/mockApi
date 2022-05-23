3f3b065d436bb7c568a8253253ab34cd7613686f
lewispoe <lewis.poe@gmail.com>
2022-05-23 12:11:19 +0100

    Quick clean up of middleware function

--- src/express.js
+++ src/express.js
@@ -1,32 +1,29 @@
 import express from 'express'
-import cors from "cors"
 import https from 'https'
 import fs from 'fs'
 import {fakeData} from "./account.js";
 
 const app = express()
-app.use(cors())
+const clientAuthMiddleware = () => (req, res, next) => {
+    if (!req.client.authorized) {
+        console.log('unauthorized')
+        // return res.status(401).send('Invalid client certificate authentication.');
+    }
+    return next();
+};
+
+app.use(clientAuthMiddleware());
 const port = 3001
 
 app.get('/', (req, res) => {
-    if (!req.client.authorized) {
-        console.log('Request is invalid')
-        console.log(req)
-        return res.status(401).send('Invalid client certificate authentication.');
-    }
-
     res.send('Hello World!')
 })
 
 app.get('/account', (req, res) => {
-    if (!req.client.authorized) {
-        console.log('Request is invalid')
-        // console.log(req)
-        // return res.status(401).send('Invalid client certificate authentication.');
-    }
-    
     res.send(JSON.stringify(fakeData()))
 })
+
+
 
 
 https
